2e1c6d9f7ef9c1a06071ac5f1acabe9c39dd2867
dedoyle <dedoyle@163.com>
2021-01-27 23:33:16 +0800

    优化渲染，显示颜色岛，键盘事件监听

--- index.js
+++ index.js
@@ -1,66 +1,107 @@
+// es6 module，变量只在模块内有效
+// 暂时不考虑事件移除
+
+// 横向格子数量
+let width = 0
+// 纵向格子数量
+let height = 0
+// 收集颜色格子坐标 grid[x][y] = 1
+// x 为纵轴，y 为 横轴
+let grid = []
+let islandNumEl = null
+let numTimer = null
+
 window.onload = () => {
   const btn = document.getElementById('btn')
+  islandNumEl = document.getElementById('island-num')
 
   btn.addEventListener('click', handleBtnClick)
 
-  function handleBtnClick() {
-    const width = Math.floor(document.getElementById('width-num').value)
-    const height = Math.floor(document.getElementById('height-num').value)
-
-    if (!width || !height || width < 0 || height < 0) {
-      alert('请输入宽度和高度，且必须输入正整数')
-      return
-    }
-
-    generateCells(width, height)
-
-    document.oncontextmenu = function (e) {
-      e.preventDefault()
-    }
-    const panel = document.getElementById('panel')
-    panel.onmousedown = handleBoxClick
+  document.oncontextmenu = function (e) {
+    e.preventDefault()
   }
+  document.addEventListener('keydown', handleKeyDown)
 }
 
-function generateCells(width, height) {
-  console.log(width, height)
-  const data = getData(width, height)
-  console.log(data)
+function handleBtnClick() {
+  width = Math.floor(document.getElementById('width-num').value)
+  height = Math.floor(document.getElementById('height-num').value)
+
+  if (!width || !height || width < 0 || height < 0) {
+    alert('请输入宽度和高度，且必须输入正整数')
+    return
+  }
+
+  initCells(width, height)
+}
+
+function initCells(width, height) {
   hideForm()
+  const data = getData(width, height)
   drawCells(data, width, height)
+
+  document
+    .getElementById('panel')
+    .addEventListener('mousedown', handleCellClick)
 }
 
 function getData(width, height) {
   const data = []
-  for (let i = 0, len = width * height; i < len; i++) {
-    data.push({
-      id: i,
-      color: '',
-    })
+  for (let x = 0; x < height; x++) {
+    grid[x] = []
+    for (let y = 0; y < width; y++) {
+      // 每个格子颜色初始化为 0，表示无颜色
+      grid[x][y] = 0
+      data.push({
+        x,
+        y,
+        color: '',
+      })
+    }
   }
   return data
 }
 
 function drawCells(data, width, height) {
-  const fragment = document.createDocumentFragment()
-  let outter
-  let inner
-  data.forEach((item) => {
-    outter = document.createElement('div')
-    outter.classList.add('cell')
-    outter.style = `width: ${(1 / width) * 100}%; height: ${
-      (1 / height) * 100
-    }%;`
-    outter.dataset.key = item.id
-
-    inner = document.createElement('div')
-    inner.classList.add('w-full', 'h-full', 'bd-1')
-    outter.append(inner)
-    fragment.append(outter)
-  })
+  const copy = [...data]
+  let per = 100
   const panel = document.getElementById('panel')
   panel.classList.remove('hidden')
-  panel.append(fragment)
+
+  function drawLines() {
+    const part = copy.splice(0, per)
+    if (part.length === 0) {
+      return
+    }
+
+    const fragement = document.createDocumentFragment()
+
+    let cell = null
+    part.forEach((item) => {
+      cell = document.createElement('div')
+      cell.setAttribute('id', `key-${item.x}-${item.y}`)
+      cell.classList.add(
+        'cell',
+        'bd-1',
+        'bd-solid',
+        'bd-grey',
+        'inline-block',
+        'transition-colors'
+      )
+      cell.style = `width: ${(1 / width) * 100}%; height: ${
+        (1 / height) * 100
+      }%;`
+      cell.dataset.x = item.x
+      cell.dataset.y = item.y
+      fragement.append(cell)
+    })
+
+    panel.append(fragement)
+
+    requestAnimationFrame(drawLines)
+  }
+
+  requestAnimationFrame(drawLines)
 }
 
 function hideForm() {
@@ -69,23 +110,81 @@ function hideForm() {
 
 // 生成随机颜色
 function getRandomColor() {
-  return (
-    '#' +
-    (function (color) {
-      return (color += '0123456789abcdef'[Math.floor(Math.random() * 16)]) &&
-        color.length == 6
-        ? color
-        : arguments.callee(color)
-    })('')
-  )
+  let color = '#'
+  //随机生成 6 位 0-15 之间的数字，再用 toString(16) 将数字转成 16进制
+  for (let i = 0; i < 6; i++) {
+    color += parseInt(Math.random() * 16).toString(16)
+  }
+  return color
 }
 
 // 点击盒子填充或清除颜色
-function handleBoxClick(ev) {
-  const el = ev.target
-  if (ev.button === 0) {
+function handleCellClick(e) {
+  const el = e.target
+  if (!el.classList.contains('cell')) {
+    return
+  }
+  const { x, y } = el.dataset
+  if (e.button === 0) {
     el.style.backgroundColor = getRandomColor()
-  } else if (ev.button === 2) {
+    grid[x][y] = 1
+  } else if (e.button === 2) {
     el.style.backgroundColor = 'unset'
+    grid[x][y] = 0
+  }
+
+  getIslandNum(grid)
+}
+
+function getIslandNum(grid) {
+  function dfs(grid, i, j) {
+    if (i < 0 || i > height || j < 0 || j > width || grid[i][j] === 0) {
+      return
+    }
+    grid[i][j] = 0
+    dfs(grid, i - 1, j)
+    dfs(grid, i + 1, j)
+    dfs(grid, i, j - 1)
+    dfs(grid, i, j + 1)
+  }
+  let count = 0
+  let copy = JSON.parse(JSON.stringify(grid))
+  for (let i = 0; i < height; i++) {
+    for (let j = 0; j < width; j++) {
+      if (copy[i][j] === 1) {
+        dfs(copy, i, j)
+        count++
+      }
+    }
+  }
+  showIslandNum(count)
+  return count
+}
+
+function showIslandNum(num) {
+  clearTimeout(numTimer)
+  islandNumEl.classList.remove('opacity-0')
+  islandNumEl.classList.add('opacity-1')
+  islandNumEl.innerText = '' + num
+  numTimer = setTimeout(() => {
+    islandNumEl.classList.remove('opacity-1')
+    islandNumEl.classList.add('opacity-0')
+  }, 1000)
+}
+
+function handleKeyDown(e) {
+  const keyNum = window.event ? e.keyCode : e.which
+  // c 对应的键码为 67
+  if (keyNum !== 67) {
+    return
+  }
+  for (let x = 0; x < height; x++) {
+    for (let y = 0; y < width; y++) {
+      if (grid[x][y] === 1) {
+        document.getElementById(
+          `key-${x}-${y}`
+        ).style.backgroundColor = getRandomColor()
+      }
+    }
   }
 }
